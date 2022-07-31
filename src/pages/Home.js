0eaebe4790887f603ea55198aa13d516d4681517
DMarrs <diegomarrs@gmail.com>
2022-07-31 16:36:59 -0700

    theme toggling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Nav from '../components/Navbar'
 
 function Home() {
+
+    const [mode, setMode] = useState('dark')
+
     return (
-        <Nav/>
+        <div className={mode}>
+            <Nav/>
+            <button onClick={() => mode == 'dark' ? setMode('') : setMode('dark')}>
+                Toggle Theme
+            </button>
+        </div>
     );
 }
 
