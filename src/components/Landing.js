210c3e802c04022c02027b2332366836610a3c56
DMarrs <diegomarrs@gmail.com>
2022-08-04 15:52:30 -0700

    responsive tweaks

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -32,17 +32,17 @@ function Landing() {
     return (
         <section className="fixed w-full top-0 z-0 dark:text-white dark:bg-gray-800 h-screen flex items-center">
             <animated.div style={fadeIn} className="flex flex-row md:flex-row-reverse flex-wrap mx-auto justify-center">
-                <div className="max-w-sm px-4">
+                <div className="max-w-xs md:max-w-sm px-4">
                     <img className="py-3 rounded-full" src={profilePic}/>
                 </div>
-                <div className="max-w-xl px-4 flex items-center">
+                <div className="max-w-xl px-5 flex items-center">
                     <div>
-                        <p className="text-2xl py-3">Looking for full stack web developer talent?</p>
-                        <h1 className="text-6xl font-semibold py-3">Hello, I'm Diego Marrs</h1>
-                        <p className="text-2xl py-3">I'm a full stack web developer who is proficient in various technologies.</p>
+                        <p className="text-lg md:text-2xl py-2 md:py-3">Looking for full stack web developer talent?</p>
+                        <h1 className="text-3xl md:text-6xl font-semibold py-2 md:py-3">Hello, I'm Diego Marrs</h1>
+                        <p className="text-lg md:text-2xl py-2 md:py-3">I'm a full stack web developer who is proficient in various technologies.</p>
                         <div className="flex flex-row flex-wrap">
                             {icons.map((icon, i) => 
-                                <img className="p-2" key={i} width="65px" src={icon}></img>
+                                <img className="p-1 md:p-2 w-12 md:w-16" key={i} src={icon}></img>
                             )}
                         </div>
                     </div>
