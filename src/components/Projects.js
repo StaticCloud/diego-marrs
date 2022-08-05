29971b4564f0c606f95b244d26926e04d9288605
DMarrs <diegomarrs@gmail.com>
2022-08-04 18:15:14 -0700

    animated projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSpring, useTrail, animated } from 'react-spring';
+import { useInView } from 'react-intersection-observer';
 
 function Projects() {
+
+    const { ref, inView } = useInView();
+
+    useEffect(() => {
+        console.log(inView)
+    }, [inView])
 
     const pages = [
         'Beet Hub',
@@ -9,14 +17,19 @@ function Projects() {
         'Password Generator'
     ]
 
+    const trail = useTrail(pages.length, {
+        opacity: inView ? 0.5 : 0,
+        marginLeft: inView ? 0 : 50
+    })
+
     return (
         <section className="min-h-screen flex flex-col w-full dark:bg-gray-900 dark:text-white py-20 z-40 relative">
             <div className="flex items-center">
                 <h1 className="text-3xl md:text-6xl font-semibold inline-block p-4 mx-auto">Projects</h1>
             </div>
-            <div className="flex-1 flex flex-col justify-between py-9">
-                {pages.map((page, i) => 
-                    <h1 key={i} className="transition-all duration-200 text-6xl md:text-8xl font-semibold px-6 opacity-50 hover:opacity-100">{page}</h1>
+            <div ref={ref} className="flex-1 flex flex-col justify-between py-9">
+                {trail.map(({ opacity, marginLeft }, i) => 
+                    <animated.h1 style={{ marginLeft, opacity }} key={pages[i]} className={`text-6xl md:text-8xl font-semibold px-6`}>{pages[i]}</animated.h1>
                 )}
             </div>
         </section>
