530cb02e2938ad17f45c2087a06e9dbf15f34ca9
DMarrs <diegomarrs@gmail.com>
2022-08-04 19:39:06 -0700

    once more updated animations for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { useSpring, useTrail, animated } from 'react-spring';
+import React from 'react';
 import { useInView } from 'react-intersection-observer';
 
 function Projects() {
@@ -13,27 +12,20 @@ function Projects() {
         'Password Generator'
     ]
 
-    const trail = useTrail(pages.length, {
-        opacity: inView ? 1 : 0,
-        marginLeft: inView ? 0 : 50
-    })
-
     return (
         <section className="min-h-screen flex flex-col w-full dark:bg-gray-900 dark:text-white py-20 z-40 relative">
             <div className="flex items-center">
                 <h1 className="text-3xl md:text-6xl font-semibold inline-block p-4 mx-auto">Projects</h1>
             </div>
             <div ref={ref} className="flex-1 flex flex-col justify-between py-9">
-                {trail.map(({ opacity, marginLeft }, i) => 
-                    <animated.h1 
+                {pages.map((page, i) => 
+                    <h1 
                         key={pages[i]} 
-                        className={`text-6xl md:text-8xl font-semibold px-6`}
-                        style={{ 
-                            marginLeft, 
-                            opacity
-                        }}>
-                           {pages[i]}
-                        </animated.h1>
+                        className={`transition-all duration-500
+                                    ${inView ? 'opacity-50 px-6' : 'opacity-0 px-10'} 
+                                    text-6xl md:text-8xl font-semibold hover:opacity-100`}>
+                           {page}
+                    </h1>
                 )}
             </div>
         </section>
