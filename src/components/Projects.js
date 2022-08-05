aaab5384ceffbaee4323585e77048e0189ea62f0
DMarrs <diegomarrs@gmail.com>
2022-08-04 21:18:43 -0700

    elements now properly animate in view thanks to InView

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { useInView } from 'react-intersection-observer';
+import { InView } from 'react-intersection-observer';
 
 function Projects() {
-
-    const { ref, inView } = useInView();
 
     const pages = [
         'Beet Hub',
@@ -17,15 +15,19 @@ function Projects() {
             <div className="flex items-center">
                 <h1 className="text-3xl md:text-6xl font-semibold inline-block p-4 mx-auto">Projects</h1>
             </div>
-            <div ref={ref} className="flex-1 flex flex-col justify-between py-9">
+            <div className="flex-1 flex flex-col justify-between py-9">
                 {pages.map((page, i) => 
-                    <h1 
-                        key={pages[i]} 
-                        className={`transition-all duration-500
-                                    ${inView ? 'opacity-50 px-6' : 'opacity-0 px-10'} 
-                                    text-6xl md:text-8xl font-semibold hover:opacity-100`}>
-                           {page}
-                    </h1>
+                    <InView threshold="1">
+                        {({ inView, ref }) => (
+                            <h1 ref={ref} key={pages[i]} 
+                                className={`transition-all duration-500
+                                        ${inView ? 'opacity-50 px-6' : 'opacity-0 px-10'} 
+                                        hover:delay-75
+                                        text-6xl md:text-8xl font-semibold hover:opacity-100`}>
+                                {page}
+                            </h1>
+                        )}
+                    </InView>
                 )}
             </div>
         </section>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { useInView, useRef } from 'react-intersection-observer';
+import { useSpring, animated } from 'react-spring';
 
 import Prev from '../icons/ResumePrev.png';
 
 function Resume() {
+
+    const [ref, inView] = useInView({
+        threshold: 0.7
+    });
+
+    const fade = useSpring({
+        opacity: inView ? 1 : 0,
+        y: inView ? 0 : 50
+
+    })
+
     return (
         <section className="min-h-screen flex flex-col w-full dark:bg-gray-900 dark:text-white py-20 z-40 relative">
             <div className="flex items-center">
                 <h1 className="text-3xl md:text-6xl font-semibold inline-block p-4 mx-auto">Resume</h1>
             </div>
-            <div className="flex-1 flex flex-wrap md:flex-row-reverse items-center h-full justify-center">
+            <animated.div style={fade} ref={ref} className="flex-1 flex flex-wrap md:flex-row-reverse items-center h-full justify-center">
                 <div className="max-w-md px-6">
                     <img src={Prev} className="p-4"></img>
                 </div>
@@ -17,7 +30,7 @@ function Resume() {
                     <p className="text-lg md:text-2xl py-3">Feel free to view my resume to learn more about my projects, work history, qualifications, and more.</p>
                     <p className="text-xl md:text-3xl py-3 text-red-600">Download</p>
                 </div>
-            </div>
+            </animated.div>
         </section>
     );
 }
