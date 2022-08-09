e4c9b1d1675c57101fb2f2b416d16a5b5b23f677
DMarrs <diegomarrs@gmail.com>
2022-08-08 20:25:48 -0700

    changes to responsive layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,9 @@ import React from 'react';
 function Navbar() {
     return (
         <nav className="fixed w-full top-0 flex flex-wrap p-4 backdrop-blur-sm dark:text-white transition-all duration-700 z-50">
-            <h1 className="flex-1 font-semibold text-6xl px-5">Diego Marrs</h1>
-            <div className="flex-1 flex items-center justify-end px-5">
+            <h1 className="flex-1 hidden md:block font-semibold text-6xl px-5">Diego Marrs</h1>
+            <h1 className="flex-1 md:hidden font-semibold text-6xl px-5">DM</h1>
+            <div className="flex-1 hidden md:flex items-center justify-end px-5">
                 <ul className="flex">
                     <li className="px-4 text-2xl">about</li>
                     <li className="px-4 text-2xl">projects</li>
--- src/components/Project.js
+++ src/components/Project.js
@@ -9,24 +9,24 @@ function Project({ size, page }) {
                             ${size === 0 ? 'h-0' : ''}`}>
                 <div className={`max-w-2xl px-6 select-none`}>
                     <img src={page.image}
-                         className="m-4 rounded-3xl"></img>
+                         className="md:m-4 rounded-3xl"></img>
                 </div>
                 <div className={`max-w-2xl px-6 select-none`}>
                     <h1 className="text-3xl md:text-6xl font-semibold py-3">{page.title}</h1>
-                    <div className="flex">
+                    <div className="flex flex-wrap">
                         {page.stack.map((technology, i) => 
-                            <p key={i} className="pr-1 text-gray-400">
+                            <p key={i} className="pr-1 text-gray-400 text-sm md:text-lg">
                                 {technology} 
                                 {i < page.stack.length - 1 ? ' |' : ''}
                             </p>
                         )}
                     </div>
                     <p className="text-lg md:text-2xl py-3">{page.description}</p>
-                    <a className="transition-all my-1 mr-3 text-2xl text-gray-400 hover:text-white" href={page.app} target="_blank">
+                    <a className="transition-all my-1 mr-3 text-lg md:text-2xl text-gray-400 hover:text-white" href={page.app} target="_blank">
                         Deployed App
                     </a>
                     <p className="inline-flex text-2xl"> | </p>
-                    <a className="transition-all my-1 ml-3 text-2xl text-gray-400 hover:text-white" href={page.github} target="_blank">
+                    <a className="transition-all my-1 ml-3 text-lg md:text-2xl text-gray-400 hover:text-white" href={page.github} target="_blank">
                         GitHub
                     </a>
                 </div>
