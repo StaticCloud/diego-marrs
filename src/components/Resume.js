7c146041acbe11316c39d21546d2ce43a7cea436
DMarrs <diegomarrs@gmail.com>
2022-08-08 20:15:34 -0700

    added resume download

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useInView } from 'react-intersection-observer';
 import { useSpring, animated } from 'react-spring';
 
+import resume from '../icons/DIEGO MARRS.pdf'
+
 import Prev from '../icons/ResumePrev.png';
 
 function Resume() {
@@ -28,7 +30,7 @@ function Resume() {
                 <div className="max-w-xl px-6">
                     <h1 className="text-3xl md:text-6xl font-semibold py-3">Learn more about me and my work!</h1>
                     <p className="text-lg md:text-2xl py-3">Feel free to view my resume to learn more about my projects, work history, qualifications, and more.</p>
-                    <p className="text-xl md:text-3xl py-3 text-red-600">Download</p>
+                    <a href={resume} target="_blank" className="transition-all select-none hover:cursor-pointer text-xl md:text-3xl py-3 text-gray-500 hover:text-white">Download</a>
                 </div>
             </animated.div>
         </section>
