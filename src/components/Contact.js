ef898e827993a6b078bffba5dab241fe24332b90
DMarrs <diegomarrs@gmail.com>
2023-02-08 10:23:09 -0800

    added calendy

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,15 +5,17 @@ import { useSpring, animated } from 'react-spring';
 import email from '../icons/svgs/envelope-solid.svg';
 import linkedin from '../icons/svgs/linkedin-in.svg';
 import github from '../icons/svgs/github.svg';
+import calendy from '../icons/svgs/calendy.svg';
 
 import coffee from '../icons/coffee.jpg'
 
 function Contact() {
 
     const contactDetails = [
+        [calendy, "https://calendly.com/diegomarrs/30min"],
         [email, "diegomarrs@proton.me"],
         [linkedin, "https://www.linkedin.com/in/diego-marrs/"],
-        [github, "https://github.com/StaticCloud"]
+        [github, "https://github.com/StaticCloud"],
     ];
 
     const [ref, inView] = useInView({
@@ -37,15 +39,15 @@ function Contact() {
                 <div className="max-w-xl px-5 flex items-center">
                     <div>
                         <h1 className="text-3xl md:text-6xl font-semibold py-2 md:py-3">Let's get in touch!</h1>
-                        <p className="text-lg md:text-2xl py-2 md:py-3 text-gray-400">Whether you're looking to add new talent to your team, or just wish to make an introduction and stay connected, I'd be happy to chat!</p>
+                        <p className="text-lg py-2 md:py-3 text-gray-400">Whether you're looking to add new talent to your team, or just wish to make an introduction and stay connected, I'd be happy to chat!</p>
                         <div className="flex flex-col">
                             {contactDetails.map((contact, i) => 
                                 <div key={i} className="flex">
                                     <img className="p-1 md:p-2 w-10 md:w-12" src={contact[0]}></img>
                                     {contact[1] != "diegomarrs@proton.me" ? (
-                                        <a href={contact[1]} target="_blank" className="text-lg md:text-2xl py-3 md:py-3 px-2 hover:cursor-pointer transition-all text-gray-400 hover:text-white">{contact[1].split('//')[1]}</a>
+                                        <a href={contact[1]} target="_blank" className="text-lg py-3 md:py-3 px-2 hover:cursor-pointer transition-all text-gray-400 hover:text-white">{contact[1].split('//')[1]}</a>
                                     ) : (
-                                        <a href={`mailto:${contact[1]}`} target="_blank" className="text-lg md:text-2xl py-3 md:py-3 px-2 hover:cursor-pointer transition-all text-gray-400 hover:text-white">{contact[1]}</a>
+                                        <a href={`mailto:${contact[1]}`} target="_blank" className="text-lg py-3 md:py-3 px-2 hover:cursor-pointer transition-all text-gray-400 hover:text-white">{contact[1]}</a>
                                     )}
                                     
                                 </div>
