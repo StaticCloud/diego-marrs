969a0fb02cc386dc07a65321062bb86b96bcc7c7
StaticCloud <diegomarrs@gmail.com>
2024-11-14 20:59:42 -0800

    refined tests to only test behavior and input; modified test command

--- src/tests/Section.test.tsx
+++ src/tests/Section.test.tsx
@@ -4,18 +4,6 @@ import Section from '../components/Section';
 import 'jest-styled-components';
 
 describe('Section Component Tests', () => {
-    it('Should render a section with desired styling.', () => {
-        render(
-            <Section>
-                <p>Hello world!</p>
-            </Section>
-        );
-
-        const section = document.querySelector('section');
-    
-        expect(section).toHaveStyleRule('background-color', '#1E3554');
-    })
-
     it('Should render a section with one child element.', () => {
         render(
             <Section>
@@ -38,5 +26,24 @@ describe('Section Component Tests', () => {
         const section = document.querySelector('section');
 
         expect(section).toHaveStyleRule('background-image', 'test.png');
+    })
+
+    it('Should render a section with multiple child elements.', () => {
+        render(
+            <Section>
+                <h1>Heading 1</h1>
+                <h2>Heading 2</h2>
+                <span>
+                    <p>Paragraph</p>
+                </span>
+            </Section>
+        );
+
+        const section = document.querySelector('section');
+
+        expect(section).toContain(document.querySelector('h1'));
+        expect(section).toContain(document.querySelector('h2'));
+        expect(section).toContain(document.querySelector('span'));
+        expect(section).toContain(document.querySelector('p'));
     })
 })
